31774eec7729a277a21b12ecaa724ba257e9ccb2
coffeineAkky <coffeineakky896@gmail.com>
2026-07-04 17:06:06 +0900

    APIで1件取得できるようになった。

--- src/app/api/StatsTableId/route.ts
+++ src/app/api/StatsTableId/route.ts
@@ -3,18 +3,19 @@ export async function GET() {
 
   const params = new URLSearchParams({
     appId: APP_ID,
-    statsDataId: "0003378613",   // 統計表ID
-    // cdArea: "13000",             // 例: 東京都
-    // limit: "100",
+    searchKind: "1",   // 統計表ID
+    searchWord: "%輸出%",
+    // surveyYears: "202010",
+    // limit: "1000",
   });
 
   const res = await fetch(
-    `https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?${params}`
+    `https://api.e-stat.go.jp/rest/3.0/app/json/getStatsList?${params}`
   );
   const json = await res.json();
 
   // 実データは VALUE 配列に入っている
-  const values = json.GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE;
+  const values = json.GET_STATS_LIST.DATALIST_INF.TABLE_INF;
   // 各要素: { "@area": "13000", "@time": "2020...", "$": "14047594" } のような形
   console.log(values);
   return Response.json(values);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,50 @@
 
 import { Aladin } from "next/font/google";
 import Image from "next/image";
+import { useState } from "react";
+
 
 export default function Home() {
- const BASE_URL = "https://api.e-stat.go.jp/rest/3.0/app/json";
 
-  const handleCallApi = async () => {
-    const response = await fetch("/api/stats");
-    alert(response);
+  const [tableInfo, setTableInfo ] = useState<any[]>([]);
 
-    // http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=&lang=J&statsDataId=0003425295&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0
+  const handleGetTableId = async () => {
+  const res = await fetch("/api/StatsTableId");
+  const tableInfoData = await res.json();
+    console.log(tableInfo);
+    setTableInfo(tableInfoData);
+  };
+
+  const handleGetMetaInfo = async () => {
+  const res = await fetch("/api/metaInfo");
+  const json = await res.json();
+    console.log(json);
+  };
+
+  const handleGetStats = async () => {
+  const res = await fetch("/api/stats");
+  const json = await res.json();
+    console.log(json);
   };
 
   return (
     <div>
-      <button type="button" onClick={handleCallApi}>
-        処理実行
+      <button type="button" onClick={handleGetTableId}>
+        統計表のIDを取得
       </button>
+      <button type="button" onClick={handleGetMetaInfo}>
+        メタ情報取得
+      </button>
+      <button type="button" onClick={handleGetStats}>
+        統計情報取得
+      </button>
+    <ul>
+      {tableInfo.map((table) => (
+        <li key={table["@id"]}>
+          {table["@id"]}: {table.TITLE.$}
+        </li>
+      ))}
+    </ul>
     </div>
   );
 }
