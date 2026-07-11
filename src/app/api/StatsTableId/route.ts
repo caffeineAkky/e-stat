970b055a7ef3ac19efe22f1a2f8ec566f0982f29
coffeineAkky <coffeineakky896@gmail.com>
2026-07-11 21:42:26 +0900

    国、品目、年固定で取得データを表示できるように

--- src/app/api/StatsTableId/route.ts
+++ src/app/api/StatsTableId/route.ts
@@ -1,13 +1,16 @@
 export async function GET() {
   const APP_ID = process.env.ESTAT_APP_ID!;
 
+  const getYears = [2020, 2021, 2022, 2023, 2024, 2025];
+
+  // for ()
   const getTableIdParams = new URLSearchParams({
     appId: APP_ID,
-    // searchKind: "1",   // 統計表ID
+    searchKind: "1",   // 統計表ID
     // searchWord: "%輸出%",
     statsCode: "00350300",
     searchWord: "概況品別国別表 輸出",
-    // surveyYears: "202010",
+    surveyYears: "202010",
     // limit: "1000",
   });
 
@@ -55,10 +58,22 @@ export async function GET() {
   );
   const json = await res.json();
 
-  // 実データは VALUE 配列に入っている
-  const values = json.GET_STATS_DATA.STATISTICAL_DATA.CLASS_INF.CLASS_OBJ;
-  // 各要素: { "@area": "13000", "@time": "2020...", "$": "14047594" } のような形
-  console.log(values);
+  const values = json.GET_STATS_DATA.STATISTICAL_DATA;
+   console.log(values);
+
+   const constConfigOfResultLabel = {
+
+   };
+
+   const fetchedData = values.DATA_INF.VALUE;
+
+  //  for (let i = 0; i < fetchedData.length; i++) {
+
+  //  };
+
+   //returnの形は、{ {国名(country):codeとname、年次(year):年次、品目名(item): }, {合計数量、合計金額、各月数量、各月金額}}としたい。
+
+   
   return Response.json(values);
 
 }
---
+++ src/lib/estat/definitions/trade.ts
@@ -0,0 +1,6 @@
+// cat01など分かりにくい名前の対応表を作成
+export const tradeCategories = {
+    cat02: {
+        
+    }
+}
